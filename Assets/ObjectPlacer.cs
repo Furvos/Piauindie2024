4778688f1fd5f823b3b15735253ba90841c74ca4
Nicolas Armando <nicolas-armando@hotmail.com>
2024-11-30 12:37:42 -0300

    SoPreuVerUmaCoisa

--- Assets/ObjectPlacer.cs
+++ Assets/ObjectPlacer.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class ObjectPlacer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+	private void Start()
+	{
+		GetComponent
+	}
+	void FixedUpdate()
     {
-        
-    }
+		Vector3 centerOfSphere = sphere.transform.position;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		Vector3 placementPosition = placePosition;
+
+		Vector3 normal = (placementPosition - centerOfSphere).normalized;
+	}
 }
